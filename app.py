678cb923ad3219eef6c212c8268bb482ade5d54d
myzm-cg <mary.muench@capgroup.com>
2023-01-23 19:27:39 +0000

    connection idea- fix later

--- app.py
+++ app.py
@@ -12,4 +12,32 @@ conn = psycopg2.connect(
 
 connection.set_session(autocommit=True)
 
-# add actions here
+# Actions:
+
+# OPEN AN ACCOUNT
+# check if account already exists, and check auto-increment:
+def open_account(address: string, city, street, zipcode, firstName, lastName, email, openingBalance: int) -> bool:
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            INSERT into Address 
+            
+            SELECT AddressID from Address order by AddressID desc limit 1
+
+
+            INSERT into Customer
+
+            SELECT CustomerID from Customer where 
+            
+            
+            INSERT into Account 
+            SET (AccountNumber, CustomerID, CurrentBalance) 
+            VALUES (%(accountNum)s, %(customerID)s, %(openingBalance)s);
+            
+            """)
+
+            
+
+        
+
+    admin, = result
+    return admin
