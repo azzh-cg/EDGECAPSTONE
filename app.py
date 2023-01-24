9299d87a8d75d4169db907192fb3554f5db4c411
myzm-cg <mary.muench@capgroup.com>
2023-01-24 17:18:32 +0000

    Actions

--- app.py
+++ app.py
@@ -14,30 +14,105 @@ connection.set_session(autocommit=True)
 
 # Actions:
 
+#deserialize json
+#temp_addr = new Address(address, city, street, zipcode)
+#temp_cust = new Customer(firstName, lastName, email)
+#temp_acc = new Account(openingBalance)
+
 # OPEN AN ACCOUNT
 # check if account already exists, and check auto-increment:
-def open_account(address: string, city, street, zipcode, firstName, lastName, email, openingBalance: int) -> bool:
+def open_account(address: string, city, street, zipcode, firstName, lastName, email, openingBalance: int):  
     with connection.cursor() as cursor:
         cursor.execute("""
             INSERT into Address 
-            
-            SELECT AddressID from Address order by AddressID desc limit 1
+            SET (Address, City, Street, Zipcode)
+            VALUES (%(address)s, %(city)s, %(street)s, %(zipcode)s,)
+            """)
+        connection.commit()
+        addressID = cursor.fetchone().Address_ID
 
-
+        cursor.execute("""
             INSERT into Customer
+            SET (first_name, last_name, Address_ID, email)
+            VALUES (%(firstName)s, %(lastName)s, %(addressID)s, %(email)s,)
+            """)
+        connection.commit()
+        customerID = cursor.fetchone().Customer_ID
 
-            SELECT CustomerID from Customer where 
             
-            
+        cursor.execute(""" 
             INSERT into Account 
-            SET (AccountNumber, CustomerID, CurrentBalance) 
-            VALUES (%(accountNum)s, %(customerID)s, %(openingBalance)s);
-            
+            SET (Customer_ID, current_balance) 
+            VALUES (%(customerID)s, %(openingBalance)s);
+            """)
+        connection.commit()
+
+def retrieve_all_accounts():
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT * FROM Account
             """)
 
-            
+def retrieve_specific_account(accountNum):
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT * FROM Account WHERE account_number = %(accountNum)s
+            """)
 
-        
+def execute_withdrawal(accountNum, withdrawal):
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT current_balance FROM Account WHERE account_number = %(accountNum)s
+            """)
+        current_balance = cursor.fetchone().current_balance
+        if(current_balance >= withdrawal):
+            cursor.execute("""
+            UPDATE Account 
+            SET current_balance 
+            VALUES (%(current_balance)s - %(withdrawal)s);
+            """)
+            connection.commit()
 
-    admin, = result
-    return admin
+def execute_deposit(accountNum, deposit):
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT current_balance FROM Account WHERE account_number = %(accountNum)s
+            """)
+        current_balance = cursor.fetchone().current_balance
+        cursor.execute("""
+            UPDATE Account 
+            SET current_balance 
+            VALUES (%(current_balance)s + %(deposit)s);
+            """)
+        connection.commit()
+
+def close_account(accountNum:
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("""
+            SELECT Customer_ID from Account where account_num = %(accountNum)s
+            """)
+            customerId = cursor.fetchone().Customer_ID
+
+            cursor.execute("""
+            DELETE from Account where account_num = %(accountNum)s
+            """)
+            connection.commit()
+
+            cursor.execute("""
+            SELECT Address_ID from Customer where Customer_ID = %(customerId)s
+            """)
+            addressId = cursor.fetchone().Address_ID
+
+            cursor.execute("""
+            DELETE from Customer where Customer_ID = %(customerId)s
+            """)
+            connection.commit()
+
+            cursor.execute("""
+            DELETE from Address where Address_ID = %(addressId)s
+            """)
+            connection.commit()
+
+    catch (Exception, psycopg2.Error) as error:
+        throw Exception("Account you are trying to close does not exist")
