559f1319fdece6905e15d1ab5aff2348b96802da
pedroh2416 <pedroh2416@gmail.com>
2023-01-25 18:52:55 +0000

    Fixed account.py in services

--- bank/services/account.py
+++ bank/services/account.py
@@ -3,15 +3,18 @@ from bank.models.account import Account
 from bank.repositories.account import AccountRepository
 
 
+
 class AccountService():
     def __init__(self, account_repository: AccountRepository):
         self.account_repository = account_repository
 
-    def add_new(self, account: Account)
+    def add_new(self, account: Account):
         self.inserted_address = self.address_repository.insert(account.customer.address)
-        self.inserted_customer = self.customer_repository.insert(account.customer.address = inserted_address)
-        self.inserted_account = self.account_repository(account.customer = inserted_customer)
-        return inserted_account
+        account.customer.address = self.inserted_address
+        self.inserted_customer = self.customer_repository.insert(account.customer)
+        account.customer = self.inserted_customer
+        self.inserted_account = self.account_repository(account.customer)
+        return self.inserted_account
 
     def get_one(self, account_number):
         account = self.account_repository.get_by_number(account_number)
