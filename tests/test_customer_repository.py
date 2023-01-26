09d31aef185276fcd8fd9875bca58ac86519d936
pedroh2416 <pedroh2416@gmail.com>
2023-01-26 16:36:06 +0000

    added init files

--- tests/test_customer_repository.py
+++ tests/test_customer_repository.py
@@ -18,7 +18,7 @@ class TestCustomerRepository(unittest.TestCase):
         self.inserted_customer = self.customerRepository.insert(Customer(id=0, first_name = "Jane", last_name="Doe", address = inserted_address, email = "janedoe@gmail.com") )
         self.assertEqual("Jane", self.inserted_customer.first_name)
 
-    def test_delete(self):
+    #def test_delete(self):
         #_address = new Address(id=0, address="1234 Lane Rd", city='Los Angeles', state='California', zipcode=90210)
         #self.inserted_customer = self.customerRepository.insert(Customer(id=0, first_name = "Jane", last_name="Doe", address = _address, email = "janedoe@gmail.com") )
         #self.customerRepository.delete(inserted_customer.id)
